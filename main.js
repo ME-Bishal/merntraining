3128ef170fcf72ce4bd1712855bddb7cb20eef84
ME-Bishal <hapiebishal@gmail.com>
2025-07-09 00:12:35 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// const sum = (a, b) => {
+//   return a % b;
+// };
+//
+// console.log(sum(1, 2));
+// console.warn("Enter the value");
+// const b =0;
+// var c;
+// let a;
+// function Test() {
+//   a = 10;
+//   c = 11;
+//   return a + b + c;
+// }
+// console.log(Test(a, b, c));
+// console.log(Test(a, b, c));
+
+// let percentage = prompt("Enter a value");
+// let a=document.getElementsByTagName("body[0]"){
+//   a.body.style.backgroundColor="red"
+// }
+let percentage = 90;
+if (percentage >= 90) {
+  console.log("You can cook minute rice in 59 seconds");
+} else if (percentage >= 80 && percentage <= 90) {
+  console.log("You are Asian");
+} else if (percentage >= 70 && percentage <= 80) {
+  console.log("You are a Csian");
+} else {
+  console.log("Failure");
+}
+console.log(document.title);
